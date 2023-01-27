0dad38802d4cf078d3c7e614a0de4d7d0ccbaa66
binh <khanhbinh1186@gmail.com>
2023-01-27 14:31:40 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss';
+import Header from './components/Header';
+import TableUsers from './components/TableUsers';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <TableUsers />
+    </div>
+  );
+}
+
+export default App;
